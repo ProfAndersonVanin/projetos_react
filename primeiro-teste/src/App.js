fd31b247237b3350bd363c01cdc4443f115c764b
profandersonvanin <profandersonvanin@gmail.com>
2025-08-08 17:18:06 -0300

    Criação do componentes e CSS

--- primeiro-teste/src/App.js
+++ primeiro-teste/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Frase from './components/Frase';
+import DigaMeuNome from './components/DigaMeuNome';
 
 function App() {
+
+  
   return (
     <div className="App">
       <h1>MEU APP REACT</h1>
-      <p>sajdfhasdfhasdfh</p>
+      <Frase />
+      <HelloWorld />
+      <Frase />
+      <DigaMeuNome nome="FULANO" />
+      <p>Outro paragrafo</p>
     </div>
   );
 }
